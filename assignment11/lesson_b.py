1b5908f46e4ed4cb0fb01a5d09d704be55baa271
belunatic <abelluvyou@yahoo.com>
2026-09-09 17:06:25 -0700

    reset back to the original code given

--- assignment11/lesson_b.py
+++ assignment11/lesson_b.py
@@ -3,10 +3,8 @@ import plotly.data as pldata
 
 
 df = pldata.iris(return_type='pandas') # Returns a DataFrame.  plotly.data has a number of sample datasets included.
-print(df.head())
-print(df.info())
-fig = px.scatter(df, x='sepal_width', y='petal_width', color='species',
-                 title="Iris Data, Sepal vs. Petal Length", hover_data=["species", "species_id"])
+fig = px.scatter(df, x='sepal_length', y='petal_length', color='species',
+                 title="Iris Data, Sepal vs. Petal Length", hover_data=["petal_length"])
 fig.write_html("iris.html", auto_open=True)
 
 # Do not try fig.show()!  This sometimes works, but usually it just hangs.
