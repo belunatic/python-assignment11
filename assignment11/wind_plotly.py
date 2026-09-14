871d39ac7619f81f78774836088c223c68e32171
belunatic <abelluvyou@yahoo.com>
2026-09-13 20:59:02 -0700

    update question and wind_plotly.py

--- assignment11/wind_plotly.py
+++ assignment11/wind_plotly.py
@@ -2,11 +2,12 @@ import plotly.express as px
 import plotly.data as pldata
 df = pldata.wind(return_type='pandas')
 
-print(df.head(10))
+print(df.head(30))
 print(df.tail(10))
+print(df.info())
 
 # Extract numeric part and convert to float
-df['strength'] = df['strength'].str.extract(r'^(\d+)').astype(float)  
+df['strength'] = df['strength'].str.extract(r'^(\d+)').astype(float)
 
 fig = px.scatter(df, x='strength', y='frequency', color='direction',
                  title="Wind Data, Strength vs. Frequency")
